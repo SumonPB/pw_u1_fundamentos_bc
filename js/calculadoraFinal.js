6b1fd7b4766d061d828c2cd76d633fa4b1e89181
SumonPB <byroncondolo99@gmail.com>
2025-11-07 19:36:07 -0500

    CalculadoraFinal

---
+++ js/calculadoraFinal.js
@@ -0,0 +1,32 @@
+let num1=0;
+let num2=0;
+
+function mostrarDisplay(valor){
+    let elemento = document.getElementById('display');
+    elemento.innerText = elemento.innerText + valor;
+    
+}
+
+function sumar(num1, num2){ 
+
+}
+
+function restar(){
+
+}
+function multiplicar(){
+
+}
+function dividir(){
+
+}
+function porcentaje(){
+
+}
+
+function clearDisplay(){
+    let elemento = document.getElementById('display');
+    elemento.innerText = "";
+}
+
+
